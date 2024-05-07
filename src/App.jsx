5cf75425fb825cd8ea493f41bf6891df20678d3a
tomtarpeyyt <ttarpey58@gmail.com>
2024-05-07 18:37:06 +0100

    updated layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import Dashboard from './components/Dashboard';
@@ -11,13 +11,40 @@ function App() {
  return (
   <>
   <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/login" element={ <LoginForm /> } />
-    <Route path="/register" element={ <RegisterForm /> } />
+    <Route path="/" element={<Layout />}>
+    <Route index element={<HomePage />} />
+    <Route path="dashboard" element={<Dashboard />} />
+    <Route path="login" element={ <LoginForm /> } />
+    <Route path="register" element={ <RegisterForm /> } />
   </Routes>
   </>
 )
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App
