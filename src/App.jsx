7cb9b23ef37b8749a194eb4be306fd522c9c3ea6
tomtarpeyyt <ttarpey58@gmail.com>
2024-05-07 18:53:04 +0100

    updated layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import Dashboard from './components/Dashboard';
 import HomePage from './components/HomePage';
-import Layout from './components/Layout';
 
 
 
@@ -12,11 +11,10 @@ function App() {
  return (
   <>
   <Routes>
-    <Route path="/" element={<Layout />} />
-    <Route index element={<HomePage />} />
-    <Route path="dashboard" element={<Dashboard />} />
-    <Route path="login" element={ <LoginForm /> } />
-    <Route path="register" element={ <RegisterForm /> } />
+    <Route path="/" element={<HomePage />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/login" element={ <LoginForm /> } />
+    <Route path="/register" element={ <RegisterForm /> } />
   </Routes>
   </>
 )
